ceda490fa2f0db93630e55db2d60fdb110ef235c
Harsha Vardhan <vummadiharsha123@gmail.com>
2025-07-27 16:18:51 -0400

    Initial commit: Set up Spring Boot project with core dependencies

---
+++ src/main/java/com/supplytrack/SupplytrackApplication.java
@@ -0,0 +1,13 @@
+package com.supplytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplytrackApplication.class, args);
+	}
+
+}
